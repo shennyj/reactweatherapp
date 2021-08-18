91b1800887e1b7fe5cac294e7ccce618fa403ede
John Shen <johnshen101@yahoo.com>
2021-08-18 12:51:51 -0400

    last commit

--- src/components/Input.js
+++ src/components/Input.js
@@ -2,31 +2,27 @@ import React,{useState} from 'react'
 import Weather from './Weather'
 
 function Input(){
-    const [input,setInput] = useState('')
     const [location,setLocation] = useState('')
     const [weather,setWeather]  = useState({})
     const [onSubmit,setOnSubmit] = useState(false)
 
     const searchLocation = (e) =>{
         setOnSubmit(true)
-        setLocation(input)
         e.preventDefault()
-        console.log(location)
-       
+        
           fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=${apiKey}`)
             .then(res=>res.json())
             .then(data=>{
-                
-              setWeather(data)//data is an object
+              setWeather(data)
+              setLocation('')
+              
             })
         
         
       }
     
       const apiKey = '0a330a454a1ad0ff544079653191e957'
-      const handleInput = (e) => {
-            setInput(e.target.value)
-      }
+    
     return(
         <div>
 
@@ -38,8 +34,8 @@ function Input(){
                 type="text"
                 className="search-bar"
                 placeholder="Search for City..."
-                onChange={handleInput}
-                value={input}
+                onChange={e=>setLocation(e.target.value)}
+                value={location}
                 />
                 <button onClick={searchLocation} className="search-button">SEARCH</button>
         </form>
